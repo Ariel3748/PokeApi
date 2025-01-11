8817f47f9f0f052cdc9663038747b2a2b43753a6
Ariel3748 <ariel.oliva3748@gmail.com>
2025-01-11 20:29:10 -0300

    Agrego descripcion de los pokemons
    Ahora al generar un pokemon tambien se muestra su descripcion en español

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const linkApi =  "https://pokeapi.co/api/v2/pokemon/"
+
+
 function mostrarPokemon(data){
     let htmlPagina = document.querySelector(".poke-card");
     var tipos = data.types.map(typeInfo => typeInfo.type.name);
@@ -8,12 +11,24 @@ function mostrarPokemon(data){
         <div class="tipos">
         <h3 class="tipo">${tipos.map(element => {return element.charAt(0).toUpperCase() + element.slice(1);}).join(" ")}</h4>
         </div>
-        <p class="descripcion">"Descripcion en proceso"</p>
       </div>
     `;
     
-    htmlPagina.innerHTML = html;
+    htmlPagina.innerHTML += html;
 
+}
+
+function mostrarDescripcionPokemon(data){
+  const parrafo = document.createElement("p");
+  const htmlPagina = document.querySelector(".poke-card");
+  const descripcion = `${data.flavor_text_entries.find(entry => entry.language.name === 'es').flavor_text}`;
+  console.log(descripcion)
+
+  parrafo.classList.add("descripcion");
+  parrafo.textContent = descripcion;
+
+  // Agregar el párrafo al contenedor
+  htmlPagina.appendChild(parrafo);
 }
 
 function NumeroRandom(){
@@ -23,19 +38,18 @@ function NumeroRandom(){
 
 
 function mostrarPokemonRandom(){
-const link = `${linkApi}` + `${NumeroRandom()}`
-console.log(link)
+const randomNumber = NumeroRandom()
+console.log(randomNumber)
+const link = `${linkApi}` + `${randomNumber}`
 fetch(link)
 .then(resp => resp.json())
 .then(data => mostrarPokemon(data))
+//Descripcion
+const linkDescripcion = `https://pokeapi.co/api/v2/pokemon-species/${randomNumber}/`
+fetch(linkDescripcion)
+.then(resp => resp.json())
+.then(data => mostrarDescripcionPokemon(data))
 }
 
 
 
-
-const linkApi =  "https://pokeapi.co/api/v2/pokemon/"
-
-
-//Agregar el listener
-
-const btn = document.querySelector("#btn-generate");
